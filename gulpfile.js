94915d31e64a9491c534dcd3cf602b377f8a13de
kensukegoto <utogoken2@yahoo.co.jp>
2019-02-17 18:02:10 +0900

    [chore] ディレクトリ構成の変更

--- gulpfile.js
+++ gulpfile.js
@@ -11,38 +11,23 @@ var mqpacker = require('css-mqpacker');
 var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('sass', function(){
-  gulp.src(['./_src/sass/**/*.scss', './_src/sass/**/*.sass'])
-    .pipe(sourcemaps.init())
+  gulp.src(['./*.scss'])
     .pipe(plumber())
-    .pipe(sassLint({
-      configFile: '.scss-lint.yml'
-    }))
     .pipe(sass({outputStyle: 'expanded'}))
     .on('error', notify.onError(function(err) {
       return err.message;
     }))
     .pipe(postcss([autoprefixer({browsers: ['> 2%']})]))
     .pipe(postcss([mqpacker()]))
-    .pipe(gulp.dest('./css'))
+    .pipe(gulp.dest('./'))
 });
 
 gulp.task('watch', function(){
-  watch(['./_src/sass/**/*.scss', './_src/sass/**/*.sass'], function() {
+  watch(['./*.scss'], function() {
     return gulp.start(['sass']);
   });
 })
 
 
-gulp.task('sass-release', function() {
-  gulp.src(['./_src/sass/**/*.scss', './_src/sass/**/*.sass'])
-    .pipe(plumber())
-    .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(postcss([autoprefixer({browsers: ['> 2%']})]))
-    .pipe(postcss([cssdeclsort({order: 'smacss'})]))
-    .pipe(postcss([mqpacker()]))
-    .pipe(gulp.dest('./css'))
-})
-
-
 gulp.task('default',['watch']);
-gulp.task('release',['sass-release']);
+
